4da409dfedd87560c23daad3eab0d69dc4751a68
mschuetz <markus_schuetz@gmx.at>
2015-03-17 15:55:39 +0100

    fixed writing intensity to bin files

--- PotreeConverter/src/PointAttributes.cpp
+++ PotreeConverter/src/PointAttributes.cpp
@@ -3,7 +3,7 @@
 
 const PointAttribute PointAttribute::POSITION_CARTESIAN		= PointAttribute(0, "POSITION_CARTESIAN",	3, 12);
 const PointAttribute PointAttribute::COLOR_PACKED			= PointAttribute(1, "COLOR_PACKED",			4, 4);
-const PointAttribute PointAttribute::INTENSITY				= PointAttribute(2, "INTENSITY",			1, 1);
+const PointAttribute PointAttribute::INTENSITY				= PointAttribute(2, "INTENSITY",			1, 2);
 const PointAttribute PointAttribute::CLASSIFICATION			= PointAttribute(3, "CLASSIFICATION",		1, 1);
 
 bool operator==(const PointAttribute& lhs, const PointAttribute& rhs){ 
