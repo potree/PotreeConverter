f3f7599253d5d975a124c3434c2d325bccb96933
Federico Russo <russo.federico@anteash.com>
2015-03-23 15:58:01 +0100

    neighbours vector uses reserve(26) and isDistant() uses squared values.

--- PotreeConverter/src/SparseGrid.cpp
+++ PotreeConverter/src/SparseGrid.cpp
@@ -15,7 +15,7 @@ SparseGrid::SparseGrid(AABB aabb, float spacing){
 	this->width = (int)(aabb.size.x / spacing);
 	this->height = (int)(aabb.size.y / spacing);
 	this->depth = (int)(aabb.size.z / spacing);
-	this->spacing = spacing;
+	this->squaredSpacing = spacing * spacing;
 	numAccepted = 0;
 }
 
@@ -51,48 +51,18 @@ vector<GridCell*> SparseGrid::targetArea(int x, int y, int z){
 
 bool SparseGrid::isDistant(const Vector3<double> &p, int i, int j, int k){
 	GridCell *cell = this->operator[](GridIndex(i,j,k));
-	if(cell->minGap(p) < spacing){
+	if(cell->squaredMinGap(p) < squaredSpacing){
 		return false;
 	}
 	for(int a = 0; a < cell->neighbours.size(); a++){
 		GridCell *neighbour = cell->neighbours[a];
-		float gap = neighbour->minGap(p);
-		if(gap < spacing){
+		float gap = neighbour->squaredMinGap(p);
+		if(gap < squaredSpacing){
 			return false;
 		}
 	}
 
 	return true;
-}
-
-float SparseGrid::minGap(const Vector3<double> &p){
-	int nx = (int)(width*(p.x - aabb.min.x) / aabb.size.x);
-	int ny = (int)(height*(p.y - aabb.min.y) / aabb.size.y);
-	int nz = (int)(depth*(p.z - aabb.min.z) / aabb.size.z);
-
-	int i = min(nx, width-1);
-	int j = min(ny, height-1);
-	int k = min(nz, depth-1);
-
-	GridIndex index(i,j,k);
-	if(find(index) == end()){
-		this->operator[](index) = new GridCell(this, index);
-	}
-
-	return minGap(p, i, j, k);
-}
-
-float SparseGrid::minGap(const Vector3<double> &p, int i, int j, int k){
-	GridCell *cell = this->operator[](GridIndex(i,j,k));
-	float minGap = MAX_FLOAT;
-
-	minGap = min(cell->minGap(p), minGap);
-	for(int a = 0; a < cell->neighbours.size(); a++){
-		GridCell *neighbour = cell->neighbours[a];
-		minGap = min(minGap, neighbour->minGap(p));
-	}
-
-	return minGap;
 }
 
 bool SparseGrid::add(Vector3<double> &p){
