ab236391560e6f18706974f7ecc5fb9cee40d55c
m-schuetz <mschuetz@potree.org>
2020-08-19 14:05:31 +0200

    replace visual studio files with cmake build

--- Converter/libs/laszip/cmake/modules.cmake
+++
@@ -1,5 +0,0 @@
-#
-# Processing necessary to deal with modules.
-#
-set(CMAKE_MODULE_PATH ${LASZIP_MODULE_DIR} ${CMAKE_MODULE_PATH})
-
--- Converter/libs/laszip/cmake/policies.cmake
+++
@@ -1,6 +0,0 @@
-#
-# cmake policies
-#
-if (CMAKE_MAJOR_VERSION GREATER 2)
-    cmake_policy(SET CMP0042 NEW) # osx rpath
-endif()
