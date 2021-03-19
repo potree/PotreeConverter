193f647247e602bfe079bc0ad64a8f633e964317
Alex Knoll <alex@innateagency.com>
2021-03-19 08:37:38 -0600

    Handle extended classifications.

--- Converter/src/chunker_countsort_laszip.cpp
+++ Converter/src/chunker_countsort_laszip.cpp
@@ -459,10 +459,19 @@ namespace chunker_countsort_laszip {
 			int offsetClassification = outputAttributes.getOffset("classification");
 			Attribute* attributeClassification = outputAttributes.get("classification");
 			auto classification = [data, point, header, offsetClassification, attributeClassification](int64_t offset) {
-				data[offset + offsetClassification] = point->classification;
+                if (point->extended_classification > 31)
+                    {
+                        data[offset + offsetClassification] = point->extended_classification;
 
-				attributeClassification->min.x = std::min(attributeClassification->min.x, double(point->classification));
-				attributeClassification->max.x = std::max(attributeClassification->max.x, double(point->classification));
+                        attributeClassification->min.x = std::min(attributeClassification->min.x, double(point->extended_classification));
+                        attributeClassification->max.x = std::max(attributeClassification->max.x, double(point->extended_classification));
+				    }
+				    else {
+				        data[offset + offsetClassification] = point->classification;
+
+                        attributeClassification->min.x = std::min(attributeClassification->min.x, double(point->classification));
+                        attributeClassification->max.x = std::max(attributeClassification->max.x, double(point->classification));
+				    }
 			};
 
 			int offsetSourceId = outputAttributes.getOffset("point source id");
