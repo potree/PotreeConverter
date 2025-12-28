9def4f911031910546e38845b850332fbab2e15a
zjyhjqs <zjyhjqs@Hotmail.com>
2025-12-28 23:10:57 +0800

    feat: add monitor support for macOS

--- Converter/modules/unsuck/unsuck_platform_specific.cpp
+++ Converter/modules/unsuck/unsuck_platform_specific.cpp
@@ -1,7 +1,9 @@
 #include "unsuck.hpp"
 
+#include <cstddef>
 #include <cstdint>
 #include <chrono>
+#include <algorithm>
 #include <iostream>
 #include <thread>
 
@@ -311,5 +313,173 @@ CpuData getCpuData() {
 	return data;
 }
 
+#elif defined(__APPLE__) && defined(__MACH__)
+
+#include <mach/host_info.h>
+#include <mach/kern_return.h>
+#include <mach/mach.h>
+#include <mach/mach_host.h>
+#include <mach/mach_init.h>
+#include <mach/mach_port.h>
+#include <mach/mach_vm.h>
+#include <mach/machine.h>
+#include <mach/message.h>
+#include <mach/port.h>
+#include <mach/shared_region.h>
+#include <mach/task.h>
+#include <mach/task_info.h>
+#include <mach/vm_prot.h>
+#include <mach/vm_region.h>
+#include <mach/vm_statistics.h>
+#include <sys/sysctl.h>
+#include <unistd.h>
+
+namespace {
+
+struct MachPort {
+	mach_port_t port;
+
+	~MachPort() {
+		if (port != MACH_PORT_NULL) {
+			mach_port_deallocate(mach_task_self(), port);
+		}
+	}
+};
+
+auto getMachHostPort() {
+	static auto machHost = MachPort{ mach_host_self() };
+	return machHost.port;
+}
+
+} // namespace
+
+MemoryData getMemoryData() {
+	const auto pageSize = sysconf(_SC_PAGESIZE);
+	if (pageSize < 0) {
+		return {};
+	}
+
+	MemoryData data;
+
+	{
+		size_t physMem = 0;
+		auto length = sizeof(physMem);
+		int mib[2] = { CTL_HW, HW_MEMSIZE };
+		if (sysctl(mib, 2, &physMem, &length, nullptr, 0) == 0) {
+			data.physical_total = physMem;
+		}
+	}
+	{
+		xsw_usage vmusage;
+		size_t size = sizeof(vmusage);
+		int mib[2] = { CTL_VM, VM_SWAPUSAGE };
+		if (sysctl(mib, 2, &vmusage, &size, nullptr, 0) == 0) {
+			data.virtual_used = vmusage.xsu_used;
+			
+			// Note: Unlike most UNIX-based operating systems, OS X does not use a preallocated disk partition for the backing store. Instead, it uses all of the available space on the machine’s boot partition.
+			// https://developer.apple.com/library/archive/documentation/Performance/Conceptual/ManagingMemory/Articles/AboutMemory.html
+			// https://superuser.com/questions/1396826/macos-virtual-memory-limit-architectural-or-practical
+			data.virtual_total = vmusage.xsu_total;
+		}
+	}
+	auto count = HOST_VM_INFO_COUNT;
+	vm_statistics_data_t vm_stat;
+	if (host_statistics(getMachHostPort(), HOST_VM_INFO, (host_info_t)&vm_stat, &count)
+		== KERN_SUCCESS)
+	{
+		const auto usedMem =
+			((size_t)vm_stat.active_count + vm_stat.inactive_count + vm_stat.wire_count) * pageSize;
+		// const auto freeMem = vm_stat.free_count * (size_t)pageSize;
+
+		data.physical_used = usedMem;
+	}
+	{
+		mach_task_basic_info info;
+		mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
+		if (task_info(mach_task_self(), MACH_TASK_BASIC_INFO, (task_info_t)&info, &infoCount)
+			== KERN_SUCCESS) {
+			data.physical_usedByProcess = info.resident_size;
+		}
+	}
+	{ // https://stackoverflow.com/questions/78815207/why-is-virtual-size-on-macos-so-different-from-vmsize-on-linux
+		const auto isSharedRegion = [](mach_vm_address_t address) {
+			return address >= SHARED_REGION_BASE && address < (SHARED_REGION_BASE + SHARED_REGION_SIZE);
+		};
+
+		mach_vm_address_t address = 0;
+		size_t sum = 0;
+		size_t sharedSum = 0;
+		mach_vm_size_t vmsize = 0;
+		vm_region_basic_info_data_64_t info;
+		while (true) {
+			auto infoCount = VM_REGION_BASIC_INFO_COUNT_64;
+			MachPort objName;
+			if (mach_vm_region(mach_task_self(), &address, &vmsize, VM_REGION_BASIC_INFO_64, (vm_region_info_t)&info, &infoCount, &objName.port)
+				!= KERN_SUCCESS) {
+				break;
+			}
+
+			if (info.protection == VM_PROT_NONE) {
+				address += vmsize;
+				continue;
+			}
+
+			if (isSharedRegion(address)) {
+				sharedSum += vmsize;
+			}
+			address += vmsize;
+			sum += vmsize;
+		}
+		data.virtual_usedByProcess = sum - sharedSum;
+	}
+
+	static size_t physicalUsedMax = 0;
+	static size_t virtualUsedMax = 0;
+	physicalUsedMax = std::max(data.physical_usedByProcess, physicalUsedMax);
+	virtualUsedMax = std::max(data.virtual_usedByProcess, virtualUsedMax);
+	data.physical_usedByProcess_max = physicalUsedMax;
+	data.virtual_usedByProcess_max = virtualUsedMax;
+
+	return data;
+}
+
+CpuData getCpuData() {
+	static auto numProcessors = std::thread::hardware_concurrency();
+
+	CpuData data;
+	data.numProcessors = numProcessors;
+
+	struct CPULoadState {
+		size_t prevTotalTicks = 0;
+		size_t prevIdleTicks = 0;
+	};
+	static CPULoadState state;
+
+	host_cpu_load_info_data_t cpuInfo;
+	auto count = HOST_CPU_LOAD_INFO_COUNT;
+	if (host_statistics(getMachHostPort(), HOST_CPU_LOAD_INFO, (host_info_t)&cpuInfo, &count)
+		!= KERN_SUCCESS)
+	{
+		data.usage = NAN;
+		return data;
+	}
+
+	size_t totalTicks = 0;
+	for (uint32_t i = 0; i < CPU_STATE_MAX; ++i)
+	{
+		totalTicks += cpuInfo.cpu_ticks[i];
+	}
+	const auto idleTicks = cpuInfo.cpu_ticks[CPU_STATE_IDLE];
+
+	const auto totalTicksSinceLast = totalTicks - state.prevTotalTicks;
+	const auto idleTicksSinceLast = idleTicks - state.prevIdleTicks;
+
+	data.usage = (1.0 - ((double)idleTicksSinceLast / (double)totalTicksSinceLast)) * 100.0;
+
+	state.prevTotalTicks = totalTicks;
+	state.prevIdleTicks = idleTicks;
+
+	return data;
+}
 
 #endif
